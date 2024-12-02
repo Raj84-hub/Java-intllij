3de853bd2cea5ff24c01bea7685a3c61fdac7579
HP <rahangdaleraj14@gmail.com>
2024-12-02 21:27:30 +0530

    Update

--- src/prg77str.java
+++ src/prg77str.java
@@ -1,2 +1,6 @@
+// 77- Take an array of strings words and a String Prefix. Print the number of strings in words that contain pref as a prefix.
+//       Example - Input: words = ["pay","attention","practice","attend"], pref = "at"
+//                        Output: 2
+
 public class prg77str {
 }
